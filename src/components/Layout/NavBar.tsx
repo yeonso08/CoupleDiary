18490c25ebbae10f072c5e4dcc42b6b9bfc0fff1
yeonso08 <hjng0825@gmail.com>
2024-03-26 16:08:31 +0900

    feat : NavBar 스타일링 및 Sheet 추가

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,11 +1,21 @@
+import {NavLink} from "react-router-dom";
+import SheetButton from "./SheetButton.tsx";
 const NavBar = () => {
     return (
-        <div className={"bg-blue-600"}>
-            <div>
-                <div className={"text-white"}>
-                    23.12.11
+        <div className={"bg-blue-600 p-4"}>
+                <div className={"flex justify-between text-white"}>
+                    <div className={"font-semibold text-xl pl-4"}>
+                    <NavLink to={"/"}>23.12.11</NavLink>
+                    </div>
+                    <div className={" gap-10 pr-24 sm:flex hidden"}>
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"/"}>Photo</NavLink>
+                    <NavLink to={"/"}>Diary</NavLink>
+                    </div>
+                    <div className={"sm:hidden"}>
+                        <SheetButton />
+                    </div>
                 </div>
-            </div>
         </div>
     )
 }
