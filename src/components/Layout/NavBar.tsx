a59b0959ffbfa469873e672e87f5c1c5681eed40
yeonso08 <hjng0825@gmail.com>
2024-03-26 14:04:06 +0900

    feat : StoryBook 설치

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,8 +1,12 @@
 const NavBar = () => {
     return (
-        <>
-            test
-        </>
+        <div className={"bg-blue-600"}>
+            <div>
+                <div className={"text-white"}>
+                    23.12.11
+                </div>
+            </div>
+        </div>
     )
 }
 export default NavBar
