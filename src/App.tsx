a1c84056130fb2fbe61fb9022053a34e7ea0c1cf
yeonso08 <hjng0825@gmail.com>
2024-03-26 10:02:45 +0900

    feat : tailwind, shadcn/ui 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button } from "./components/ui/button"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Button>Click me</Button>
+          <p className="text-2xl font-light">The quick brown fox ...</p>
+          <p className="text-2xl font-normal">The quick brown fox ...</p>
+          <p className="text-2xl font-medium">The quick brown fox ...</p>
+          <p className="text-2xl font-semibold">The quick brown fox ...</p>
+          <p className="text-2xl font-bold">The quick brown fox ...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
