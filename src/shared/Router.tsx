8fc5dbced0495ea7a4a070928f5a73187ef26219
yeonso08 <hjng0825@gmail.com>
2024-03-26 13:47:51 +0900

    feat : NavBar 생성 및 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "../pages/Home";
+import Layout from "../components/Layout/Layout.tsx";
 
 const AppRoutes = () => {
     return (
 <Router>
     <Routes>
+        <Route element={<Layout /> }>
         <Route path={"/"} element={<Home />} />
+        </Route>
     </Routes>
 </Router>
     )
