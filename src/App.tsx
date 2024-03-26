81c03f13003690be6e403a92c43ed9f43d2e8ee6
yeonso08 <hjng0825@gmail.com>
2024-03-26 13:25:20 +0900

    feat : react-router-dom 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Button } from "./components/ui/button"
+import AppRoutes from "./shared/Router.tsx";
+
 function App() {
 
   return (
-      <div>
-          <Button>Click me</Button>
-          <p className="text-2xl font-light">The quick brown fox ...</p>
-          <p className="text-2xl font-normal">The quick brown fox ...</p>
-          <p className="text-2xl font-medium">The quick brown fox ...</p>
-          <p className="text-2xl font-semibold">The quick brown fox ...</p>
-          <p className="text-2xl font-bold">The quick brown fox ...</p>
-      </div>
+      <AppRoutes />
   )
 }
 
