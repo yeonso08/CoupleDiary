cc5fd413495871dab3df38f9a9a8faecaed861d8
yeonso08 <hjng0825@gmail.com>
2024-03-28 11:30:56 +0900

    design : 푸터 폰트 크기 변경

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="bg-[#314840] text-white text-center p-4">
+        <div className="bg-[#314840] text-white text-center p-4 text-sm border-t-0">
             <p>© 2024. HwangJaeYeon All rights reserved.</p>
         </div>
     );
